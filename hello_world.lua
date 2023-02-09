b5f2a0599c83c87fc55135351372017875326f0f
Mohamed Fawas <mohamed.da20@iiitmk.ac.in>
2023-02-09 17:29:16 +0530

    hello world functions

---
+++ hello_world.lua
@@ -0,0 +1,13 @@
+--[[
+    create a function that returns  'hello world'
+]]
+
+
+local hello_world = {}
+
+function hello_world.hello()
+    -- print("hello world ")
+    return 'Hello world!'
+end
+
+return hello_world
---
+++ hello_world_print.lua
@@ -0,0 +1,9 @@
+--[[
+    create a function that prints 'hello world'
+]]
+
+function hello_world()
+    print("Hello World!")
+end
+
+hello_world()
