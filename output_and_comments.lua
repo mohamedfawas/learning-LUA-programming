a32d58a415f9264b373fe50af7a6148c9cd4d916
Mohamed Fawas <mohamed.da20@iiitmk.ac.in>
2023-02-09 18:12:41 +0530

    output and comments in lua

---
+++ output_and_comments.lua
@@ -0,0 +1,10 @@
+-- The quotation marks ( "" or '') can be single and double. that doesn't matter. 
+print("Hello world!")
+
+--[[
+    Multiple line comment
+    line 1
+    lne 2 
+]]
+
+print("Hai " .. "Fawas".."!") -- The two dots in between is used to concatenate the strings
